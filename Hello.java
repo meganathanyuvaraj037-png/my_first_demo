8f30fad21496c6aaae9786474b386121772728c6
Yuvaraj N M <meganathanyuvaraj037@gmail.com>
2026-09-25 11:26:14 +0530

    first Hello.java class
    test file to commit changes from local to drag and drop to github

---
+++ Hello.java
@@ -0,0 +1,14 @@
+
+class Hello{
+    public static void main(String a[]){
+        byte num = 9;
+        short num1 = 129;
+        int result = num + num1;
+
+
+
+System.out.println(result);
+
+
+}
+}
