74624b05d82642880109a6267b03425e6c27ccc5
meganathanyuvaraj037-png <meganathanyuvaraj037@gmail.com>
2026-09-25 15:40:19 +0530

    added sub, mul, div to existing values

--- Hello.java
+++ Hello.java
@@ -4,10 +4,20 @@ class Hello{
         byte num = 9;
         short num1 = 129;
         int result = num + num1;
+        double result1 = num - num1;
+        int result2 = num * num1;
+        double result3= num / num1;
+    
 
 
 
 System.out.println(result);
+System.out.println(result1);
+System.out.println(result2);
+System.out.println(result3);
+
+
+
 
 
 }
